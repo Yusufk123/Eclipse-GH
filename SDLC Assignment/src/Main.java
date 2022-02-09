555e0f2507f96bff3081b3933a8af7af50984ca4
Yusufk123 <yusufk1230@gmail.com>
2022-02-09 14:38:07 -0500

    switched file reader to FileReader class

--- SDLC Assignment/src/Main.java
+++ SDLC Assignment/src/Main.java
@@ -1,19 +1,20 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
 			File txt = new File("1065.txt");
-			Scanner fileReader = new Scanner(txt);
-			while (fileReader.hasNextLine()) {
-				String data = fileReader.nextLine();
-				System.out.println(data);
+			FileReader fr = new FileReader(txt);
+			int i;
+			while (( i = fr.read()) != -1 ) {				
+	            System.out.print((char)i);
+
 			}
-			fileReader.close();
-		} catch (FileNotFoundException e) {
+			fr.close();
+		} catch (IOException e) {
 		      System.out.println("File Not Found.");
 		      e.printStackTrace();
 		    } 
