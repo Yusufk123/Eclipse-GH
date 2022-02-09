ffa9951b61a30c6619c5794c4cfbc54077cd79df
Yusufk123 <yusufk1230@gmail.com>
2022-02-09 17:53:26 -0500

    3rd try

--- SDLC Assignment/src/Main.java
+++ SDLC Assignment/src/Main.java
@@ -1,22 +1,32 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		String line;  
+        ArrayList<String> words = new ArrayList<String>();
+        
+        //File Reader
 		try {
-			File txt = new File("1065.txt");
-			FileReader fr = new FileReader(txt);
-			int i;
-			while (( i = fr.read()) != -1 ) {				
-	            System.out.print((char)i);
-
+			FileReader txt = new FileReader("1065.txt");
+			BufferedReader br = new BufferedReader(txt);
+			
+			while ((line = br.readLine()) != null) {				
+	            //System.out.print(line);
+	            String string[] = line.toLowerCase().split("([-,.\\s]+)");           
+	            for(String s : string){    
+	                words.add(s);    
+	            }
 			}
-			fr.close();
+			txt.close();
+			br.close();
 		} catch (IOException e) {
 		      System.out.println("File Not Found.");
 		      e.printStackTrace();
-		    } 
+		    }
+		
+		//Analyzer
+		
 	}
 }
