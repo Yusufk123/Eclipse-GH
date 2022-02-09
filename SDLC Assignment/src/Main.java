f5ea2158f5bd4d8f6da1e8457f171adcc0da4e75
Yusufk123 <yusufk1230@gmail.com>
2022-02-09 12:32:46 -0500

    Added File Reader

--- SDLC Assignment/src/Main.java
+++ SDLC Assignment/src/Main.java
@@ -1,9 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			File txt = new File("1065-h.htm");
+			Scanner fileReader = new Scanner(txt);
+			while (fileReader.hasNextLine()) {
+				String data = fileReader.nextLine();
+				System.out.println(data);
+			}
+			fileReader.close();
+		} catch (FileNotFoundException e) {
+		      System.out.println("File Not Found.");
+		      e.printStackTrace();
+		    }
 	}
-
 }
