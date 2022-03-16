502d5c002815dde89bd93c502db65cf18e68217f
Yusufk123 <yusufk1230@gmail.com>
2022-03-16 16:16:01 -0400

    Added a Timer in nanoseconds

--- Documentation Assignment/src/Main.java
+++ Documentation Assignment/src/Main.java
@@ -2,7 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int n;
+		int n = 21;
+		
+		System.out.println("Start Time for Recursive: " + System.nanoTime());
+		System.out.println(fibRecursive.fibR(n));
+		System.out.println("End Time for Recursive: " + System.nanoTime());
+		
+		System.out.println("Start Time for Iterative: " + System.nanoTime());
+		System.out.println(fibIterative.fibI(n));
+		System.out.println("End Time for Iterative: " + System.nanoTime());
 	}
 	
 }
