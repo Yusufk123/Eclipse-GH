e61f0faa5dc66181888ee3127a5ba27ae58b8147
Yusufk123 <yusufk1230@gmail.com>
2022-03-16 16:15:35 -0400

    Update fibIterative

--- Documentation Assignment/src/fibIterative.java
+++ Documentation Assignment/src/fibIterative.java
@@ -1,7 +1,18 @@
 
 public class fibIterative {
-	public int fibI(int n) {
-		
-		return n;
+	public static int fibI(int n) {
+		if (n <= 1) {
+			return n;
+		} else {
+			int f = 1;
+			int pf = 1;
+			
+			for(int i = 2; i < n; i++) {
+				int t = f;
+				f += pf;
+				pf = t;
+			}
+			return f;
+		}
 	}
 }
