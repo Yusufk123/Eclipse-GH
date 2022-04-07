3d891dab11bb722edb03d2dbc2357951f415fc5e
Yusufk123 <yusufk1230@gmail.com>
2022-04-06 21:39:03 -0400

    Created working single thread method and non completed multithreaded option

--- Concurrency Assignment/src/Main.java
+++ Concurrency Assignment/src/Main.java
@@ -1,9 +1,38 @@
+import java.util.Random;
 
 public class Main {
+	private static final Random RAND = new Random();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int Size = 1000;
+
+		int a[] = randomArray(Size);
+
+		System.out.println(sumSingle(a));
 
 	}
 
+	public static int[] randomArray(int length) {
+		int a[] = new int[length];
+
+		for (int i = 0; i < a.length; i++) {
+			a[i] = RAND.nextInt(50);
+		}
+		return a;
+	}
+
+	public static int sumSingle(int a[]) {
+		int answer = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			answer += a[i];
+		}
+		return answer;
+	}
+
+	public static int sumMulti(int a[]) {
+		int answer = 0;
+
+		return answer;
+	}
 }
