8471f6e7a87f6ba638b26b2d0590f7c3a2745d49
Yusufk123 <yusufk1230@gmail.com>
2022-04-06 20:58:53 -0400

    changed test cases

--- Concurrency Assignment/src/Test.java
+++ Concurrency Assignment/src/Test.java
@@ -1,10 +1,21 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.stream.IntStream;
 
 public class Test {
 
 	@org.junit.Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSingleThread() {
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int sum = IntStream.of(a).sum();
+		assertEquals(55, sum);
+	}
+
+	@org.junit.Test
+	public void testMultiThread() {
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int sum = IntStream.of(a).sum();
+		assertEquals(55, sum);
 	}
 
 }
---
+++ Concurrency Assignment/src/TestRand.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRand {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
