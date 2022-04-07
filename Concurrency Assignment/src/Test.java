a099dd4e2c3d90a5b7ccee76226112b3dfcb5e95
Yusufk123 <yusufk1230@gmail.com>
2022-04-06 21:38:11 -0400

    Created Actual Tests

--- Concurrency Assignment/src/Test.java
+++ Concurrency Assignment/src/Test.java
@@ -3,19 +3,20 @@ import static org.junit.Assert.assertEquals;
 import java.util.stream.IntStream;
 
 public class Test {
+	int a[] = Main.randomArray(20000);
 
 	@org.junit.Test
 	public void testSingleThread() {
-		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int sumT = Main.sumSingle(a);
 		int sum = IntStream.of(a).sum();
-		assertEquals(55, sum);
+		assertEquals(sumT, sum);
 	}
 
 	@org.junit.Test
 	public void testMultiThread() {
-		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int sumT = Main.sumMulti(a);
 		int sum = IntStream.of(a).sum();
-		assertEquals(55, sum);
+		assertEquals(sumT, sum);
 	}
 
 }
